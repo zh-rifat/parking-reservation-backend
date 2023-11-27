bfe175ded632578ebd1f26914ebd5062aec4637d
zh-rifat <zhrifat.00@gmail.com>
2023-11-28 01:57:43 +0600

    mm

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.listen(3000 || process.env.PORT, () => {
-  console.log('Up and running!!');
-});
+// app.listen(3000 || process.env.PORT, () => {
+//   console.log('Up and running!!');
+// });
 app.get('/api/readallcells',getAllCells);
 app.post('/api/writecell',writeData);
 app.post('/api/deletecell',deleteData);
