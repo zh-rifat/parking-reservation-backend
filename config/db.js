b5793da19ed3ec798c2220eafd071764050652d3
zh-rifat <zhrifat.00@gmail.com>
2023-11-28 02:37:50 +0600

    mm

--- config/db.js
+++ config/db.js
@@ -3,10 +3,12 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 export const auth = new google.auth.GoogleAuth({
-  keyFile: path.resolve('./credentials.json'),
+  keyFile: path.resolve('./config/credentials.json'),
   scopes: 'https://www.googleapis.com/auth/spreadsheets'
 });
 dotenv.config();
+console.log(process.cwd());
 const client = await auth.getClient();
 export const googleSheet = google.sheets({ version: 'v4', auth: client });
 export const spreadsheetId = process.env.SPREADSHEET_ID;
+
